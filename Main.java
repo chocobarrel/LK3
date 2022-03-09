795f683ee90602a4f04b2ff3124d2abfdc0d6de4
chocobarrel <101242947+chocobarrel@users.noreply.github.com>
2022-03-09 17:03:25 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+
+
+package javaapplication1;
+
+/**
+ *
+ * @author Hanif
+ */
+public class Main {
+    public static void main(String[] args) {
+            AkunBank c1 = new AkunBank("anton",100000);
+            c1.cekAkun();
+        }
+}
