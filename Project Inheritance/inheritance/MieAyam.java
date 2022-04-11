dfa7405304dbc495599267775df4204b47efc67a
chocobarrel <101242947+chocobarrel@users.noreply.github.com>
2022-04-12 04:58:48 +0700

    Update MieAyam.java

--- Project Inheritance/inheritance/MieAyam.java
+++ Project Inheritance/inheritance/MieAyam.java
@@ -21,16 +21,22 @@ public class MieAyam extends Mie {
         System.out.println("Mie Ayam direbus!");
     }
 
+    @Override
+    public void disajikan() {
+        System.out.println("Mie Ayam siap disajikan!");
+    }
+
     public void tambahSawi(int sawi) {
         this.sawi = sawi;
+        System.out.println("Menambahkan daun sawi sebanyak " + this.sawi + " lembar");
     }
 
     public void tambahAyam(int ayam) {
         this.ayam = ayam;
+        System.out.println("Menambahkan topping ayam sebanyak " + this.sawi + " gram");
     }
 
     public void disiramKuah(){
         System.out.println("Mie Ayam diberi kuah");
     }
 }
-
