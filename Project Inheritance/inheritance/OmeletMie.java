3d897d711fb0b6d19b565a6ab4fa43fbb4a54a83
chocobarrel <101242947+chocobarrel@users.noreply.github.com>
2022-04-12 04:59:54 +0700

    Update OmeletMie.java

--- Project Inheritance/inheritance/OmeletMie.java
+++ Project Inheritance/inheritance/OmeletMie.java
@@ -16,15 +16,22 @@ public class OmeletMie extends Mie {
 
     @Override
     public void direbus() {
-        System.out.println("Mie untuk omelet direbus!");
+        super.direbus();
+        System.out.println("Mie untuk omelet telah direbus!");
+    }
+
+    public void disajikan() {
+        System.out.println("Omelet mie siap disajikan!");
     }
 
     public void tambahTelur(int telur) {
         this.telur =  telur;
+        System.out.println("Menambahkan telur ke mie sebanyak " + this.telur + " butir");
     }
 
     public void tambahDaunBawang(int daunBawang){
         this.daunBawang = daunBawang;
+        System.out.println("Menambahkan daun bawang ke mie sebanyak " + this.daunBawang + " sendok");
     }
 
     public void digoreng() {
