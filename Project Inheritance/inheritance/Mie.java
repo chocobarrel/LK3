d2e6b77d39541fa6ebb2a77184e2cb05d7e96a47
chocobarrel <101242947+chocobarrel@users.noreply.github.com>
2022-04-11 13:04:57 +0700

    Update Mie.java

--- Project Inheritance/inheritance/Mie.java
+++ Project Inheritance/inheritance/Mie.java
@@ -1,5 +1,19 @@
 package inheritance;
 
 public class Mie {
+    private int berat;
+    private int porsi;
 
+    public Mie(){
+        //no-argument
+    }
+
+    public Mie(int berat, int porsi){
+        this.berat = berat;
+        this.porsi = porsi;
+    }
+
+    public void direbus() {
+        System.out.println("Mie Direbus!");
+    }
 }
